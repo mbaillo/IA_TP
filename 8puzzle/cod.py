cdb210842b04add4fd31947e6369e9422d3412b0
Mamadou <110556317+mbaillo@users.noreply.github.com>
2022-11-22 20:49:55 +0100

    Add files via upload

---
+++ 8puzzle/cod.py
@@ -0,0 +1,32 @@
+import importlib
+import time
+puzzle = importlib.import_module("8puzzle")
+import  random
+
+
+
+l = [4, 0, 2, 7, 1, 3, 8, 5, 6]
+random.shuffle(l)
+s = ' '.join(str(elem) for elem in l)
+
+goal = puzzle.EightPuzzle("1 2 3 4 5 6 7 8 0")
+initial = puzzle.EightPuzzle(s)
+h = puzzle.EightPuzzle.manhatten_distance
+h1 = puzzle.EightPuzzle.tile_switches_remaining
+# EightPuzzle.tile_switches_remaining
+# output = puzzle.EightPuzzle.action_sequence
+output = puzzle.EightPuzzle.state_transition
+
+
+
+print("Test de manhattan \n")
+start_time = time.time()
+print (initial.a_star(goal, h, output))
+temps_exec = time.time() - start_time
+print("\nL'Heuristique de manhattan à pris "+str(temps_exec)+" secondes")
+print("\nTest de Tile switch \n")
+start_time2 = time.time()
+print (initial.a_star(goal, h1, output))
+temps_exec2 = time.time() - start_time2
+print("\nL'Heuristique de Tile switch à pris "+str(temps_exec2)+" secondes")
+print("\nLa différence de ces 2 heuristiques est de "+str(temps_exec2-temps_exec)+" secondes")
